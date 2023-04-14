a816fbee793cbf7b5bc55516abf0a63fa10188aa
orltrh <aurelyataroreh@gmail.com>
2023-04-14 14:29:00 +0700

    update education

--- resources/views/users/education/empathy.blade.php
+++ resources/views/users/education/empathy.blade.php
@@ -1,64 +1,8 @@
 @extends('layout.master')
-@section('title', 'Edukasi')
-@section('menuEdukasi', 'active')
+@section('title', 'Self Awareness')
 
 @section('content')
 @parent
-<section id="edukasi" style="height:100vh; padding-top: 50px">
-    <div class="container">
-        <div class="row" style="padding-top: 100px">
-            <div class="col-sm-4 mb-3 mb-sm-0 d-flex justify-content-center">
-                <div class="book">
-                    <a href="#" class="btn btn-primary">Let's Learn</a>
-                    <div class="cover">
-                        <p>Self-Awareness</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-4 d-flex justify-content-center">
-                <div class="book">
-                    <a href="#" class="btn btn-primary">Let's Learn</a>
-                    <div class="cover">
-                        <p>Self-Regulation</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-4 d-flex justify-content-center">
-                <div class="book">
-                    <a href="#" class="btn btn-primary">Let's Learn</a>
-                    <div class="cover">
-                        <p>Self-Motivation</p>
-                    </div>
-                </div>
-            </div>
-            
-        </div>
-        <div class="row" style="padding-top: 100px">
-            <div class="col-sm-4 d-flex justify-content-center">
-                <div class="book">
-                    <a href="#" class="btn btn-primary">Let's Learn</a>
-                    <div class="cover">
-                        <p>Empathy</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-4 d-flex justify-content-center">
-                <div class="book">
-                    <a href="#" class="btn btn-primary">Let's Learn</a>
-                    <div class="cover">
-                        <p>Social Skills</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-4 d-flex justify-content-center">
-                <div class="book">
-                    <a href="#" class="btn btn-primary">Let's Learn</a>
-                    <div class="cover">
-                        <p>Self-Adjustment</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-@endsection
+
+<p>Halaman empathy</p>
+
---
+++ resources/views/users/education/socialskills.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.master')
+@section('title', 'Self Awareness')
+
+@section('content')
+@parent
+
+<p>halaman social skills</p>
+
+

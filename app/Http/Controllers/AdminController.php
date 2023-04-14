95be90a1b39bad7b056861551385bd901ed443c4
Azarya Aditya Krisna Moeljono <azaryamoeljono12@gmail.com>
2023-04-14 12:15:02 +0700

    appaja

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function index(){
+        return view('admin.dashboardAdmin');
+    }
+    public function tracking(){
+        return view('admin.tracking');
+    }
+    public function panduan(){
+        return view('admin.panduan');
+    }
+    public function forum(){
+        return view('admin.forum');
+    }
+    public function edukasi(){
+        return view('admin.edukasi');
+    }
+    public function updateEdukasi(){
+        return view('admin.updateEdukasi');
+    }
+}
--- resources/views/forum.blade.php
+++ resources/views/forum.blade.php
@@ -4,36 +4,19 @@
 
 @section('content')
 @parent
-<section style="height:76.5vh; padding-top: 100px">
-    <div class="container">
-        <div class="card text-center">
-            <div class="card-header">
-              Forum chat 1
-            </div>
-            <div class="card-body">
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-            <div class="card-footer text-muted">
-              2 days ago
-            </div>
+<section style="height:100vh; padding-top: 100px">
+    <div class="container d-flex justify-content-center">
+      <div class="cardforum">
+        <div class="chat-headerforum">Forum</div>
+          <div class="chat-windowforum">
+            <ul class="message-list"></ul>
           </div>
-    </div>
-    <div class="container mt-5">
-        <div class="card text-center">
-            <div class="card-header">
-              Forum chat 2
-            </div>
-            <div class="card-body">
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-            <div class="card-footer text-muted">
-              2 days ago
-            </div>
+          <div class="chat-inputforum">
+              <input type="text" class="message-input" placeholder="Type your message here">
+              <button class="send-button">Send</button>
           </div>
+        </div>
+      
     </div>
 </section>
 @endsection

186042232b6e850b7dc41da006cf337da1d856eb
Azarya Aditya Krisna Moeljono <azaryamoeljono12@gmail.com>
2023-04-14 11:25:10 +0700

    new

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,70 @@
+@extends('layout.master')
+@section('title', 'Edukasi')
+@section('menuEdukasi', 'active')
+
+@section('content')
+@parent
+<section id="edukasi" style="height:76.5vh; padding-top: 50px">
+    <div class="container">
+        <div class="row" style="padding-top: 100px">
+            <div class="col-sm-4 mb-3 mb-sm-0">
+                <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Kesadaran Diri</h5>
+                    <p class="card-text">Self-Awareness</p>
+                    <a href="#" class="btn btn-primary">Let's Go</a>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Pengaturan Emosi</h5>
+                    <p class="card-text">Self-Regulation</p>
+                    <a href="#" class="btn btn-primary">Let's Go</a>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Motivasi Diri</h5>
+                    <p class="card-text">Self-Motivation</p>
+                    <a href="#" class="btn btn-primary">Let's Go</a>
+                </div>
+                </div>
+            </div>
+            
+        </div>
+        <div class="row" style="padding-top: 100px">
+            <div class="col-sm-4">
+                <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Empati</h5>
+                    <p class="card-text">Empathy</p>
+                    <a href="#" class="btn btn-primary">Let's Go</a>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Keterampilan Sosial</h5>
+                    <p class="card-text">Sosial Skills</p>
+                    <a href="#" class="btn btn-primary">Let's Go</a>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Penyesuaian Diri</h5>
+                    <p class="card-text">Self-Adjustment</p>
+                    <a href="#" class="btn btn-primary">Let's Go</a>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection

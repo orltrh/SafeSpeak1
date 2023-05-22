65618fca5b9967875ccfc0195555ee909d38da4a
Azarya Aditya Krisna Moeljono <azaryamoeljono12@gmail.com>
2023-05-22 10:37:00 +0700

    number valid

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -20,42 +20,52 @@ class RegisterController extends Controller
     }
 
     public function registeruser(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'username' => 'required|unique:users,username|min:3|max:255',
-            'number' => 'required|unique:users,number|max:14',
-            'email' => 'required|email:dns,rfc|max:255|unique:users,email',
-            'password' => 'required|min:8',
-            're_password' => 'required|same:password',
-            'latitude' => 'required|max:255',
-            'longitude' => 'required|max:255',
-        ]);
+{
+    $validator = Validator::make($request->all(), [
+        'username' => 'required|unique:users,username|min:3|max:255',
+        'number' => [
+            'required',
+            function ($attribute, $value, $fail) {
+                if (strpos($value, '0') === 0 && $value !== '62') {
+                    $fail('Invalid number format.');
+                }
+            },
+            'unique:users,number',
+            'max:14',
+        ],
+        'email' => 'required|email:dns,rfc|max:255|unique:users,email',
+        'password' => 'required|min:8',
+        're_password' => 'required|same:password',
+        'latitude' => 'required|max:255',
+        'longitude' => 'required|max:255',
+    ]);
 
-        if ($validator->fails()) {
-            return redirect()
-                ->back()
-                ->withErrors($validator)
-                ->withInput();
-        }
-
-        $registerlogin = new User();
-        $registerlogin->username = $request->input('username');
-        $registerlogin->number = $request->input('number');
-        $registerlogin->email = $request->input('email');
-        $registerlogin->email_verified_at = now();
-        $registerlogin->password = Hash::make($request->input('password'));
-        $registerlogin->re_password = Hash::make($request->input('re_password'));
-        $registerlogin->save();
-
-        $tracking = new TrackingPosition();
-        $tracking->username = $request->input('username');
-        $tracking->number = $request->input('number');
-        $tracking->latitude = $request->input('latitude');
-        $tracking->longitude = $request->input('longitude');
-        $tracking->save();
-
-        return redirect()->route('login');
+    if ($validator->fails()) {
+        return redirect()
+            ->back()
+            ->withErrors($validator)
+            ->withInput();
     }
+
+    $registerlogin = new User();
+    $registerlogin->username = $request->input('username');
+    $registerlogin->number = $request->input('number');
+    $registerlogin->email = $request->input('email');
+    $registerlogin->email_verified_at = now();
+    $registerlogin->password = Hash::make($request->input('password'));
+    $registerlogin->re_password = Hash::make($request->input('re_password'));
+    $registerlogin->save();
+
+    $tracking = new TrackingPosition();
+    $tracking->username = $request->input('username');
+    $tracking->number = $request->input('number');
+    $tracking->latitude = $request->input('latitude');
+    $tracking->longitude = $request->input('longitude');
+    $tracking->save();
+
+    return redirect()->route('login');
+}
+
 
     use RegistersUsers;
 
